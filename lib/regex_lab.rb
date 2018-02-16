a4a51997d7f678613c0172ced9e77f35160f9bd5
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-16 11:41:28 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,14 @@
 def starts_with_a_vowel?(word)
-
+  word.match("#{/\A+[aeiouAEIOU]/}")? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  array = text.scan("#{/\Aun/}")
+  array.grep("#{/ing\z/}")
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/(.....)$/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
